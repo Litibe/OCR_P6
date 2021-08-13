d2ecae83c3ae7ed61f8599942b639c0f70dfecaa
Lionel TISSIER <contact@litiboost.fr>
2021-08-13 16:17:57 +0200

    modif slider

--- assets/js/slideCarrousel.js
+++ assets/js/slideCarrousel.js
@@ -1,25 +1,46 @@
-class Carrousel {
-    /**
-     * @param (HTML element) element
-     * @param (Object) option
-     * @param (Object) option.slidesToScroll Nombre d'élément à scroller
-     * @param (Objetc) option.slidesVisible 
-
-     */
-    constructor (element, options = {}) {
-        this.element = element
-        this.options = Object.assign({}, {
-            slidesToScroll: 1, 
-            slidesVisible : 1
-        }, options)
+function modifVisibleSlide (idDiv, indexFirstMovie, numberMovies, slidesVisible ) {
+    let childrenCarrouselDiv = document.querySelectorAll("."+idDiv.replace("#","")+"__carrouselItem")
+    let slidesToScroll = numberMovies
+    let range = Array.from(Array(numberMovies).keys())
+    let myListMovies = Array.from(Array(numberMovies).keys())
+    console.log(myListMovies)
+    let position = indexFirstMovie
+    
+    i=0
+    while (i < slidesToScroll) {
+        childrenCarrouselDiv = document.querySelectorAll("."+idDiv.replace("#","")+"__carrouselItem")
+        childrenCarrouselDiv[myListMovies[position+i]].style.display = 'inline';
+        if (slidesVisible > 0) {
+            childrenCarrouselDiv[myListMovies[position+i]].style.display = "block";
+            slidesVisible--
+            console.log("block")
+        } else {
+            childrenCarrouselDiv[myListMovies[position+i]].style.display = "none";
+            console.log("none")
+        };
+        i++;
     }
 }
 
-document.addEventListener ('DOMContentLoaded', function() {
-    
-    new Carrousel (document.querySelector("#best_movies .carrousel"),{
-        slidesToScroll: 7, 
-        slidesVisible: 3
-    })
 
-})
+function addSliderIntoDiv (idDiv) {
+    document.addEventListener ('DOMContentLoaded', function() {
+    
+    
+    let btnLeft = document.querySelectorAll(idDiv + " .btn_carrousel")[0]
+    let btnRight = document.querySelectorAll(idDiv + " .btn_carrousel")[1]
+    let position = 0
+    modifVisibleSlide(idDiv, position, 7, 4)
+
+    btnLeft.addEventListener('click', function() {
+        position--
+        modifVisibleSlide(idDiv, position, 7, 4)
+      });
+    btnRight.addEventListener('click', function() {
+        position++
+        modifVisibleSlide(idDiv, position, 7, 4)
+      }); 
+    })
+}
+
+addSliderIntoDiv("#best_movies", 7, 3)
