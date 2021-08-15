5466c89c0c28dc46b488c268c2f68e1d2b973084
Lionel TISSIER <contact@litiboost.fr>
2021-08-15 09:48:06 +0200

    end loading

---
+++ assets/js/loading.js
@@ -0,0 +1,19 @@
+/* -------------------------------- LOADING --------------------------------  */
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('section')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 500);
+  }
+  
+  function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+  }
+  
+  onReady(function() {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+  });
+  /* -------------------------------- END LOADING --------------------------------  */
