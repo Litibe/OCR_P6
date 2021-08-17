81b515804e8834a4e283a80a8ea0fa1b0f9a477a
Lionel TISSIER <contact@litiboost.fr>
2021-08-17 18:16:17 +0200

    improve js

--- assets/js/fetchMovies.js
+++ assets/js/fetchMovies.js
@@ -1,42 +1,42 @@
-monStockage = sessionStorage;
-let adresseServeur = "http://193.26.22.227:8000/"
+let adresseServeur = "http://193.26.22.227:8000/";
 
 function addInfoBestMovie(urlBestMovie) {
   fetch(urlBestMovie)
-  .then(function(res) {if(res.ok){return res.json()}})
+  .then(function(res) {if(res.ok){return res.json()};})
   .then(function(value) {
-    let movieTitle = document.querySelector("#best_movie0 .title_movie");
-    let movieResume = document.querySelector("#best_movie0 .movieDetails .resume");
-    let movieBtnInfo = document.querySelector("#best_movie0 .movieDetails button");
-    let movieScoreImdb = document.querySelector("#best_movie0 .movieDetails p.note");
+    let movieTitle = document.querySelector(
+      "#best_movie0 .title_movie");
+    let movieResume = document.querySelector(
+      "#best_movie0 .movieDetails .resume");
+    let movieBtnInfo = document.querySelector(
+      "#best_movie0 .movieDetails button");
+    let movieScoreImdb = document.querySelector(
+      "#best_movie0 .movieDetails p.note");
     movieTitle.innerHTML = value.title;
     movieBtnInfo.classList.remove("hidden");
-    movieResume.innerHTML = "Résume du film : "
-    movieScoreImdb.innerHTML = "Note du film : " + value.imdb_score
-    let movieLongDescription = document.querySelector("#best_movie0 .movieDetails p.long_description");
-    movieLongDescription.innerHTML = "<p> "+ String(value.long_description) + "</p>"; 
-
+    movieResume.innerHTML = "Résume du film : ";
+    movieScoreImdb.innerHTML = "Note du film : "+value.imdb_score;
+    let movieLongDescription = document.querySelector(
+      "#best_movie0 .movieDetails p.long_description");
+    movieLongDescription.innerHTML = "<p> "+String(value.long_description)+"</p>"; 
     let newDivMovieMainModal = document.querySelector("#best_movie0 .modal");
-    movieBtnInfo.addEventListener('click', function() {
-      newDivMovieMainModal.style.display = "block"
-      });
-
-
-  })
+    movieBtnInfo.addEventListener("click", function() {
+      newDivMovieMainModal.style.display = "block";});
+  });
 }
 
 function extract_8_best_movies(url) {
   fetch(url)
-  .then(function(res) {if(res.ok){return res.json()}})
+  .then(function(res) {if(res.ok){return res.json();}})
   .then(function(value) {
-    let myList=[]
+    let myList=[];
     myList.push(value.results[0].url+"####"+value.results[0].imdb_score+"####"+value.results[0].votes);
     myList.push(value.results[1].url+"####"+value.results[1].imdb_score+"####"+value.results[1].votes);
     myList.push(value.results[2].url+"####"+value.results[2].imdb_score+"####"+value.results[2].votes);
     myList.push(value.results[3].url+"####"+value.results[3].imdb_score+"####"+value.results[3].votes);
     myList.push(value.results[4].url+"####"+value.results[4].imdb_score+"####"+value.results[4].votes);
     fetch(value.next)
-    .then(function(res) {if(res.ok){return res.json()}})
+    .then(function(res) {if(res.ok){return res.json();}})
     .then(function(value) {
       myList.push(value.results[0].url+"####"+value.results[0].imdb_score+"####"+value.results[0].votes);
       myList.push(value.results[1].url+"####"+value.results[1].imdb_score+"####"+value.results[1].votes);
@@ -82,20 +82,20 @@ function addMovieIntoIdDiv(idDivCarrousel, urlMovie, indexMovie){
   .then(function(res) {
     if (res.ok) {return res.json();}})
   .then(function(value) {
-    let divMovie = document.querySelector(idDivCarrousel+String(indexMovie))
+    let divMovie = document.querySelector(idDivCarrousel+String(indexMovie));
     let movieImg = document.querySelector(idDivCarrousel+String(indexMovie)+" div");
-    movieImg.innerHTML = "<img src="+value.image_url+ " alt="+ value.title + " title="+ value.title + "/>";
-    movieImg.setAttribute("id", idDivCarrousel.replace("#","")+"img_movie"+String(indexMovie))
+    let titleImg = String(value.title);
+    movieImg.innerHTML = "<img src="+value.image_url+ " alt="+ titleImg + " title="+ titleImg + "/>";
+    movieImg.setAttribute("id", idDivCarrousel.replace("#","")+"img_movie"+String(indexMovie));
     divMovie.appendChild(movieImg);
     let newDivMovieMainModal = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal");
-    let newDivMovieMainModalContent = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal-content");
     let newDivMovieMainModalContentH2 = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal-content h2");
     let newDivMovieMainModalContentInclude = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal-content .include-modal");
-    let btnExit = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal-content .btn_close")
+    let btnExit = document.querySelector(idDivCarrousel+String(indexMovie)+" .modal-content .btn_close");
     let div_modal_left = document.createElement("div");
-    div_modal_left.classList.add(idDivCarrousel+"modalLeftMovie"+String(indexMovie))
+    div_modal_left.classList.add(idDivCarrousel+"modalLeftMovie"+String(indexMovie));
     let div_modal_right= document.createElement("div");
-    div_modal_right.classList.add(idDivCarrousel+"modalRightMovie"+String(indexMovie))
+    div_modal_right.classList.add(idDivCarrousel+"modalRightMovie"+String(indexMovie));
     newDivMovieMainModalContentH2.innerHTML = value.title;
     newDivMovieMainModalContentInclude.appendChild(div_modal_left);
     let actors_movie = "";
@@ -159,13 +159,13 @@ function search_best_movie_score_vote (best_movies_url) {
   let liste_best_movies = []
   for (let [key, value] of movies_score) {
     if (value == best_score) {
-      liste_best_movies.push(key)}
+      liste_best_movies.push(key);}
   }
   let liste_votes_movies = []
   for (let movie of liste_best_movies) {
     for (let [key, value] of movies_vote) {
       if (movie == key) {
-        liste_votes_movies.push(value)
+        liste_votes_movies.push(value);
       }
     }
   }
@@ -174,7 +174,7 @@ function search_best_movie_score_vote (best_movies_url) {
   let best_vote = liste_votes_movies[liste_votes_movies.length-1]
   for (let [key, value] of movies_vote) {
     if (value == best_vote) {
-      url_best_movie = key
+      url_best_movie = key;
     }
   }
   return url_best_movie
